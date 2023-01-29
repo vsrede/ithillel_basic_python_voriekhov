cf3332eba6cc7bac9da3d94cef0740e0e5f754b0
vsrede <vsrede94@gmail.com>
2023-01-29 18:31:38 +0100

    Update task_4.py

--- homework_2/task_4.py
+++ homework_2/task_4.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-value_uah = int(input('Введіть кількість гривень '))
+value_uah = input('Введіть кількість гривень ')
 kurs_exchange = Decimal(8)
 print('Станом на 25 серпня 2022 року це становить', Decimal(value_uah) / Decimal(kurs_exchange), 'Долари США :(') 
 

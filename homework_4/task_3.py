49c7005101809d9544c0317b2b4bde67f1e2afde
vsrede <vsrede94@gmail.com>
2023-02-05 17:05:39 +0100

    Create task_3.py

---
+++ homework_4/task_3.py
@@ -0,0 +1,3 @@
+digit = int(input('    Please enter an integer number: '))
+print('    Next number for number ', digit, ' is ', digit + 1, '.', sep='')
+print('    Previous number for number ', digit, ' is ', digit - 1, '.', sep='')

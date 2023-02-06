4ed12d4ae9e245d57789704837fcefdce3c2fffa
vsrede <vsrede94@gmail.com>
2023-02-06 18:37:54 +0100

    Update task_3.py

--- homework_4/task_3.py
+++ homework_4/task_3.py
@@ -1,3 +1,3 @@
 digit = int(input('    Please enter an integer number: '))
-print('    Next number for number ', digit, ' is ', digit + 1, '.', sep='')
-print('    Previous number for number ', digit, ' is ', digit - 1, '.', sep='')
+print(f'    Next number for number {digit} is {digit + 1}.')
+print(f'    Previous number for number {digit} is {digit - 1}.')

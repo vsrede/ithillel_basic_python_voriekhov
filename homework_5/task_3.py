bcccb478988bda276ff8854eb409c28d50cabfc3
vsrede <vsrede94@gmail.com>
2023-02-09 19:55:21 +0100

    Update task_3.py

--- homework_5/task_3.py
+++ homework_5/task_3.py
@@ -7,5 +7,7 @@ def cone_square_and_volume(radius, height):  # returns 2 floats
     return s, v
 
 
-radius, height = int(input()), int(input())
-print(cone_square_and_volume(radius, height))
+radius, height = int(input('Введите радиус ')), int(input('Введите высоту '))
+s, v = cone_square_and_volume(radius, height)
+print('Площадь =', s)
+print('Объем =', v)

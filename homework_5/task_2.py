4ab00c27763ef2db9e6d25e7f8f7b159c6ef8d31
vsrede <vsrede94@gmail.com>
2023-02-09 19:52:58 +0100

    Update task_2.py

--- homework_5/task_2.py
+++ homework_5/task_2.py
@@ -3,5 +3,7 @@ def triangle_square_and_perimeter(a, b):  # returns 2 values
     c = (a**2 + b**2) ** 0.5
     p = a + b + c
     return s, p
-katet_1, katet_2 = int(input()), int(input())
-print(triangle_square_and_perimeter(katet_1, katet_2))
+katet_1, katet_2 = int(input('Введите катет ')), int(input('Введите следующий катет '))
+s, p = triangle_square_and_perimeter(katet_1, katet_2)
+print('Площадь =', s)
+print('Периметр =', p)

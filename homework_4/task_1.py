32901c390c1d69520350d8151f123dd9d2640877
vsrede <vsrede94@gmail.com>
2023-02-10 13:24:02 +0100

    Update task_1.py

--- homework_4/task_1.py
+++ homework_4/task_1.py
@@ -1,2 +1,7 @@
+#variant 1
 my_text = 'employee_first_name'
 print(my_text.title().replace('_', ''))
+#variant 2
+my_text = 'employee_first_name'
+my_text = my_text.title().replace('_', '')
+print(my_text)

27c3b747f5cd781a73c2bb696317a86389292115
vsrede <vsrede94@gmail.com>
2023-02-12 16:10:59 +0100

    Create task_4.py

---
+++ homework_6/task_4.py
@@ -0,0 +1,23 @@
+def circles_intersect(x1, y1, r1, x2, y2, r2): # returns boolean value
+    d = (((x2 - x1)**2) + ((y2 - y1)**2))**0.5
+    if r1 + r2 > d:
+        return True
+    else:
+        return False
+
+
+def main():
+    x1 = int(input('Input x1 '))
+    y1 = int(input('Input y1 '))
+    r1 = int(input('Input r1 '))
+    x2 = int(input('Input x2 '))
+    y2 = int(input('Input y2 '))
+    r2 = int(input('Input r2 '))
+    if circles_intersect(x1, y1, r1, x2, y2, r2):
+        print('circles intersect')
+    else:
+        print('circles do not intersect')
+
+
+if __name__ == '__main__':
+    main()

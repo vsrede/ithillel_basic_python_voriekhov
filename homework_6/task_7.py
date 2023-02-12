ac47a38fd9d9df5e7219c1e49b2a7a7ce044b4a7
vsrede <vsrede94@gmail.com>
2023-02-12 20:08:40 +0100

    Update task_7.py

--- homework_6/task_7.py
+++ homework_6/task_7.py
@@ -1,11 +1,18 @@
-def fibonacci(n, digit=0, pre_digit=1):
+def fibonacci(n):
+    if n <= 1:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def fibonacci_other(n, digit=0, pre_digit=1):
     if n == 1:
         return digit
-    return fibonacci(n - 1, digit=digit + pre_digit, pre_digit=digit)
+    return fibonacci_other(n - 1, digit=digit + pre_digit, pre_digit=digit)
 
 
 def main():
-    print(fibonacci(10))
+    print(fibonacci(9))
+    print(fibonacci_other(10))
 
 
 if __name__ == '__main__':

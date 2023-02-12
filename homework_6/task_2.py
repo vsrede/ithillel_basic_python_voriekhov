cf1f43aa47b63865a3f2a34576aec4886574eae0
vsrede <vsrede94@gmail.com>
2023-02-12 15:18:28 +0100

    Create task_2.py

---
+++ homework_6/task_2.py
@@ -0,0 +1,31 @@
+def solve_quadratic_equation(a, b, c):
+    discriminant = b**2 - 4 * a * c
+    x1, x2 = None, None
+    if discriminant == 0:
+        x1 = (-b + discriminant**0.5) / 2*a
+        x2 = None
+    elif discriminant > 0:
+        x1 = (-b + discriminant**0.5) / 2*a
+        x2 = (-b - discriminant**0.5) / 2*a
+    return x1, x2
+
+
+def main():
+    a = int(input('Input a '))
+    b = int(input('Input b '))
+    c = int(input('Input c '))
+    x1, x2 = solve_quadratic_equation(a, b, c)
+    if x1 is None:
+        if x2 is None:
+            print('Корней нет')
+        else:
+            print(f'x = {x2}')
+    elif x1 is not None:
+        if x2 is None:
+            print(f'x = {x1}')
+        else:
+            print(f'x1 = {x1}, x2 = {x2}')
+
+
+if __name__ == '__main__':
+    main()

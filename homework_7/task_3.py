b6f0d9c376c60a5da7fa9d69944e33b414bd3345
vsrede <vsrede94@gmail.com>
2023-02-16 21:39:30 +0100

    Create task_3.py

---
+++ homework_7/task_3.py
@@ -0,0 +1,21 @@
+def calculate_wheat_chess_position(kilograms):
+    weight_one = 1
+    weight_summ = 0
+    chessboard_letters = 'abcdefgh'
+    cell = 0
+    for i in range(1, 65):
+        weight_summ += weight_one * i
+        if weight_summ > kilograms:
+            cell = i - 1
+            break
+    letters = chessboard_letters[cell//8]
+    return chessboard_letters[cell//8], cell % 8
+
+
+def main():
+    n = float(input('Enter '))
+    print(*calculate_wheat_chess_position(n), sep='')
+
+
+if __name__ == '__main__':
+    main()

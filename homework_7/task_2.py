0b41bc2c28dd74082a26ab752c7cfcf37e4374d0
vsrede <vsrede94@gmail.com>
2023-02-16 22:02:22 +0100

    Update task_2.py

--- homework_7/task_2.py
+++ homework_7/task_2.py
@@ -1,9 +1,14 @@
-for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print('FizzBuzz')
-    elif i % 5 == 0:
-        print('Buzz')
-    elif i % 3 == 0:
-        print('Fizz')
-    else:
-        print(i)
+def main():
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print('FizzBuzz')
+        elif i % 5 == 0:
+            print('Buzz')
+        elif i % 3 == 0:
+            print('Fizz')
+        else:
+            print(i)
+
+
+if __name__ == '__main__':
+    main()

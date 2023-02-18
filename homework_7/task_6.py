0feb963ca2b615859ec46fb1d8e26baa0fa89e3d
vsrede <vsrede94@gmail.com>
2023-02-18 10:31:17 +0100

    Update task_6.py

--- homework_7/task_6.py
+++ homework_7/task_6.py
@@ -1,12 +1,15 @@
-def main():
-    idx_fib = 20
+def fibonacci_numbers(idx_fib):
     first_digit = 1
     second_digit = 0
     fib = 0
     for i in range(idx_fib):
         fib = first_digit + second_digit
         second_digit, first_digit = second_digit + first_digit, second_digit
-    print(fib)
+    return fib
+
+
+def main():
+    print(fibonacci_numbers(int(input('Enter index '))))
 
 
 if __name__ == '__main__':

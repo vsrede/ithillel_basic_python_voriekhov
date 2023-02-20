3600610bf60f3b72d34ff13109cc121a26427e03
vsrede <vsrede94@gmail.com>
2023-02-20 20:20:39 +0100

    Update task_3.py

--- homework_8/task_3.py
+++ homework_8/task_3.py
@@ -1,9 +1,11 @@
 def main():
     original_list_1 = [5, '9', 0, '452', 6.5, '6', 1, 2]
+    print(original_list_1)
     sorted_list_1 = sorted(original_list_1, key=float)
     print(sorted_list_1)
 
-    original_list_2 = ['0', 1, '1101000', '20', 326, 472, 863, 9, '99']
+    original_list_2 = [472, 326, 1, '1101000', '99', 9, '20', 863, '0']
+    print(original_list_2)
     sorted_list_2 = sorted(original_list_2, key=str)
     print(sorted_list_2)
 

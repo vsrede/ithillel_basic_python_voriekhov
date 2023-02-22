c59c61a161dac21183401c868cfb415d32fbd412
vsrede <vsrede94@gmail.com>
2023-02-22 17:00:45 +0100

    Update task_2.py

--- homework_8/task_2.py
+++ homework_8/task_2.py
@@ -1,25 +1,31 @@
 def copydeep(obj):  # returns integer or None
     lst2 = list()
-    for i in range(len(obj)):
-        if isinstance(obj[i], (str, int, float)):
-            lst2.append(obj[i])
-        elif isinstance(obj[i], list):
-            lst2.append(copydeep(obj[i]))
-        elif isinstance(obj[i], tuple):
-            lst2.append(tuple(copydeep(obj[i])))
-    return lst2
+    if isinstance(obj, (list, tuple)):
+        for i in range(len(obj)):
+            if isinstance(obj[i], (str, int, float)):
+                lst2.append(obj[i])
+            elif isinstance(obj[i], list):
+                lst2.append(copydeep(obj[i]))
+            elif isinstance(obj[i], tuple):
+                lst2.append(tuple(copydeep(obj[i])))
+        return lst2
+    else:
+        return obj
 
 
 def main():
-    lst1 = ['a', 1, 2.0, ['b', [['hard']]], [('this', ('tuple', '.'))]]
+    lst1 = ['abf', 1, 2.0, ['b', [['hard']]], [('this', ('tuple', '.'))]]
     lst2 = copydeep(lst1)
-    print(lst1)
-    print(lst2)
     lst1[3][1][0].append([])
     lst1[3][1][0].append('deep')
     print(lst1)
     print(lst2)
+    lst3 = 'Hello'
+    lst4 = copydeep(lst3)
+    print(lst4)
 
 
 if __name__ == '__main__':
     main()
+
+

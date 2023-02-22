d73b5a347d64f1ab53c7a549d5fe395b05d3d1b1
vsrede <vsrede94@gmail.com>
2023-02-22 16:42:08 +0100

    Update task_2.py

--- homework_8/task_2.py
+++ homework_8/task_2.py
@@ -3,19 +3,22 @@ def copydeep(obj):  # returns integer or None
     for i in range(len(obj)):
         if isinstance(obj[i], (str, int, float)):
             lst2.append(obj[i])
-        elif isinstance(obj[i], (list, tuple)):
-            lst2.append([])
-            for k in range(len(obj[i])):
-                lst2[i].append(obj[i][k])
-
+        elif isinstance(obj[i], list):
+            lst2.append(copydeep(obj[i]))
+        elif isinstance(obj[i], tuple):
+            lst2.append(tuple(copydeep(obj[i])))
     return lst2
 
 
 def main():
-    lst1 = ['a', 1, 2.0, ['b']]
+    lst1 = ['a', 1, 2.0, ['b', [['hard']]], [('this', ('tuple', '.'))]]
     lst2 = copydeep(lst1)
-    lst1[3].append(0)
-    print(lst1[3], lst2[3])  # ['b', 0] ['b']
+    print(lst1)
+    print(lst2)
+    lst1[3][1][0].append([])
+    lst1[3][1][0].append('deep')
+    print(lst1)
+    print(lst2)
 
 
 if __name__ == '__main__':

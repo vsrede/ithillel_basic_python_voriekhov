a0d29d3a71c96904105d3712f50c55218e34e704
vsrede <vsrede94@gmail.com>
2023-02-22 17:13:27 +0100

    Update task_4.py

--- homework_8/task_4.py
+++ homework_8/task_4.py
@@ -1,14 +1,15 @@
 def gen_primes():  # returns list of ints
     list_from_1_to_100 = list()
     for i in range(1, 101):
-        list_from_1_to_100.append(i)
+        flag = 0
+        for k in range(2, i):
+            if i % k == 0:
+                flag += 1
+        if flag == 0:
+            list_from_1_to_100.append(i)
     return list_from_1_to_100
 
 
 def main():
     my_list = gen_primes()
     print(my_list)
-
-
-if __name__ == '__main__':
-    main()

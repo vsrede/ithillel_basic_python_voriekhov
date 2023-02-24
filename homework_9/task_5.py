9facc92bb2070c3425f81e090fb89ce2690c59c9
vsrede <vsrede94@gmail.com>
2023-02-24 19:34:30 +0100

    Update task_5.py

--- homework_9/task_5.py
+++ homework_9/task_5.py
@@ -1,5 +1,6 @@
-def group_by_surname(list_of_enrollees): # returns 4 ints
+def group_by_surname(list_of_enrollees):  # returns 4 ints
     a_i_enrollee, j_p_enrollee, q_t_enrollee, u_z_enrollee = 0, 0, 0, 0
+
     for num in list_of_enrollees:
         idx = str(num).find(' ') + 1
         if 65 <= ord(str(num)[idx]) <= 73:
@@ -10,15 +11,14 @@ def group_by_surname(list_of_enrollees): # returns 4 ints
             q_t_enrollee += 1
         else:
             u_z_enrollee += 1
+
     return a_i_enrollee, j_p_enrollee, q_t_enrollee, u_z_enrollee
 
 
 def main():
-        name = ['Vadim Vadimov', 'Oleg Olegov', 'Britney Spears', 'Artem Artemov', 'Oleg Olegov']
-        print(group_by_surname(name))
+    name = ['Vadim Vadimov', 'Oleg Olegov', 'Britney Spears', 'Artem Artemov', 'Oleg Olegov']
+    print(group_by_surname(name))
 
 
 if __name__ == '__main__':
     main()
-
-

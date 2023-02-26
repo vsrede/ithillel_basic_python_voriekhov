3f9a2d74e3e71931142b90dfa79c5e36d619a448
vsrede <vsrede94@gmail.com>
2023-02-26 14:36:39 +0100

    Update task_5.py

--- homework_9/task_5.py
+++ homework_9/task_5.py
@@ -1,25 +1,24 @@
-def group_by_surname(list_of_enrollees):  # returns 4 ints
-    a_i_enrollee, j_p_enrollee, q_t_enrollee, u_z_enrollee = 0, 0, 0, 0
-    # characters acc UTF(dec)
-    a_dec, i_dec, j_dec, p_dec, q_dec, t_dec = 65, 73, 74, 80, 81, 84
+def copydeep(obj):  # returns integer or None
+    if isinstance(obj, (int, float, str)):
+        return obj
 
-    for num in list_of_enrollees:
-        first_let = ord(num[num.find(' ') + 1].upper())
-        if a_dec <= first_let <= i_dec:
-            a_i_enrollee += 1
-        elif j_dec <= first_let <= p_dec:
-            j_p_enrollee += 1
-        elif q_dec <= first_let <= t_dec:
-            q_t_enrollee += 1
-        else:
-            u_z_enrollee += 1
+    if isinstance(obj, list):
+        return [copydeep(x) for x in obj]
 
-    return a_i_enrollee, j_p_enrollee, q_t_enrollee, u_z_enrollee
+    if isinstance(obj, tuple):
+        return tuple(copydeep(x) for x in obj)
+
+    if isinstance(obj, dict):
+        return {copydeep(key): copydeep(value) for key, value in obj.items()}
 
 
 def main():
-    name = ['Vadim Vadimov', 'Oleg Olegov', 'Britney Spears', 'Artem Artemov', 'Oleg Olegov']
-    print(group_by_surname(name))
+    lst1 = ['abf', 1, 2.0, {0: 0, 1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36}]
+    lst2 = copydeep(lst1)
+    lst1[3][7] = 999
+    print(lst1)
+    print(lst2)
+    lst3 = 'Hello'
 
 
 if __name__ == '__main__':

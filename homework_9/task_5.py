ffc93552cc28796cab97f48455b13ff7b241d905
vsrede <vsrede94@gmail.com>
2023-02-26 14:12:07 +0100

    Update task_5.py

--- homework_9/task_5.py
+++ homework_9/task_5.py
@@ -1,13 +1,14 @@
 def group_by_surname(list_of_enrollees):  # returns 4 ints
     a_i_enrollee, j_p_enrollee, q_t_enrollee, u_z_enrollee = 0, 0, 0, 0
+    a_dec, i_dec, j_dec, p_dec, q_dec, t_dec = 65, 73, 74, 80, 81, 84
 
     for num in list_of_enrollees:
-        idx = str(num).find(' ') + 1
-        if 65 <= ord(str(num)[idx]) <= 73:
+        first_let = ord(num[num.find(' ') + 1].upper())
+        if a_dec <= first_let <= i_dec:
             a_i_enrollee += 1
-        elif 74 <= ord(str(num)[idx]) <= 80:
+        elif j_dec <= first_let <= p_dec:
             j_p_enrollee += 1
-        elif 81 <= ord(str(num)[idx]) <= 84:
+        elif q_dec <= first_let <= t_dec:
             q_t_enrollee += 1
         else:
             u_z_enrollee += 1

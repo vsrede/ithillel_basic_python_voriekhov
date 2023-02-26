c227e2d145ded9b14143c6a8fddb77924e990f9c
vsrede <vsrede94@gmail.com>
2023-02-26 13:49:04 +0100

    Update task_4.py

--- homework_9/task_4.py
+++ homework_9/task_4.py
@@ -1,20 +1,15 @@
 def lchain(*iterables):  # returns list
     result = list()
-    
+
     for num in iterables:
-        for k in num:
-            if isinstance(k, int):
-                result.append(k)
-            else:
-                result.extend(k)
-                
+        result.extend(num)
+
     return result
 
 
 def main():
     exeperement = ([1, 2, 3], {'5': 5}, tuple(), (6, 7), range(8, 10))
-    print(lchain(exeperement))
-    
+    print(lchain(*exeperement))
     assert lchain([1, 2, 3], {'5': 5}, tuple(), (6, 7), range(8, 10)) == [1, 2, 3, '5', 6, 7, 8, 9]
 
 

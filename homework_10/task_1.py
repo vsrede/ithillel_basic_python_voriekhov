c1e17b4f80ec54e28dddebec88e80ff3794dede7
vsrede <vsrede94@gmail.com>
2023-03-01 09:32:13 +0100

    Update task_1.py

--- homework_10/task_1.py
+++ homework_10/task_1.py
@@ -1,30 +1,31 @@
 def copydeep(obj, memory=None):  # returns integer or None
-    memory = {}
+    if memory is None:
+        memory = {}
     if id(obj) in memory:
-        return obj
-    else:
-        new_list = []
-        memory[id(obj)] = new_list
-        for idx, elem in enumerate(obj):
-            if isinstance(obj, list):
-                copydeep(elem, memory)
-            else:
-                new_list.append(elem)
+        return memory[id(obj)]
+    if isinstance(obj, list):
+        lst = list()
+        memory[id(obj)] = lst
+        for elem in obj:
+            lst.append(copydeep(elem, memory))
+        return lst
 
-        #return [copydeep(x) for x in obj]
-    if isinstance(obj, (int, float, str)):
-        return obj
-    if isinstance(obj, tuple):
-        return tuple(copydeep(x) for x in obj)
     if isinstance(obj, dict):
-        return dict(copydeep(x) for x in obj.items())
+        dict_rec = {}
+        memory[id(obj)] = dict_rec
+        for key, value in obj.items():
+            dict_rec[copydeep(key, memory)] = copydeep(value, memory)
+        return dict_rec
+    return obj
 
 
 def main():
-    lst1 = ['1']
+    lst1 = [1, 2, 3]
     lst1.append(lst1)
     lst2 = copydeep(lst1)
+    print(lst1)
     print(lst2)
+    lst3 = 'Hello'
 
 
 if __name__ == '__main__':

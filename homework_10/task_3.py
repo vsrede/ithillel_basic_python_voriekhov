a9fe3d2819e3799c4a099f58f6cbed484b249515
vsrede <vsrede94@gmail.com>
2023-03-04 22:16:21 +0100

    Update task_3.py

--- homework_10/task_3.py
+++ homework_10/task_3.py
@@ -6,18 +6,32 @@ def pemrtuate(text):  # returns permuted text
     result = list()
     for word in lst_text:
         mix_word = list()
+        new_word = list()
         if len(word) > 4:
-            three_ch = [word[1], word[2], word[3]]
-            mix_word = list(word)
-            mix_three_ch = three_ch.copy()
-            while mix_three_ch == three_ch:
-                random.shuffle(mix_three_ch)
-            mix_word[1], mix_word[2], mix_word[3] = mix_three_ch[0], mix_three_ch[1], mix_three_ch[2]
-            result.append(''.join(mix_word))
+            new_word.append(*word[0])
+            word = word[1:]
+            end_letter = word[-1]
+            word = word[:-1]
+            while True:
+                if len(word) > 2:
+                    three_ch = [word[0], word[1], word[2]]
+                    mix_word = list(word)
+                    mix_three_ch = three_ch.copy()
+                    while mix_three_ch == three_ch:
+                        random.shuffle(mix_three_ch)
+                    new_word.append(''.join(mix_three_ch))
+                    word = word[3:]
+                else:
+                    new_word.append(''.join(word))
+                    break
+            new_word.append(''.join(end_letter))
+            result.append(''.join(new_word))
         else:
             result.append(word)
 
     return result
+
+
 def main():
     my_text = 'За результатами дослідження одного англійського університету, не має значення,' \
               ' в якому порядку розташовані букви в слові. Головне, щоб перша і остання букви були на місці. ' \

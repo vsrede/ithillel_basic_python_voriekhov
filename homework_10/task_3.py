56bbc2842dc5193ae6cd204448743c2bd2db6182
vsrede <vsrede94@gmail.com>
2023-03-04 15:29:31 +0100

    Update task_3.py

--- homework_10/task_3.py
+++ homework_10/task_3.py
@@ -2,38 +2,29 @@ import random
 
 
 def pemrtuate(text):  # returns permuted text
-    text = text.lower()
-    text_lst = text.split()  #
-    text_new = list()  # for add element from text_lst
-    # add element and mix first 3 character in word
-    for i in range(len(text_lst)):
-        if len(text_lst[i]) > 3:
-            first_three_ch = [text_lst[i][0], text_lst[i][1], text_lst[i][2]]
-            mix_three_ch = first_three_ch.copy()
-            while mix_three_ch[0] == first_three_ch[0]:
+    lst_text = text.split()
+    result = list()
+    for word in lst_text:
+        mix_word = list()
+        if len(word) > 4:
+            three_ch = [word[1], word[2], word[3]]
+            mix_word = list(word)
+            mix_three_ch = three_ch.copy()
+            while mix_three_ch == three_ch:
                 random.shuffle(mix_three_ch)
-            text_new.extend(mix_three_ch)
-            text_new.extend(text_lst[i][3:])
+            mix_word[1], mix_word[2], mix_word[3] = mix_three_ch[0], mix_three_ch[1], mix_three_ch[2]
+            result.append(''.join(mix_word))
         else:
-            text_new.extend(text_lst[i])
-        text_new.extend(' ')
-    # capitalize after dot
-    for i in range(len(text_new) - 2):
-        if text_new[i] == '.':
-            text_new[i + 2] = text_new[i + 2].upper()
-    # capitalize first character
-    text_new[0] = text_new[0].upper()
-    result = ''.join(text_new)
+            result.append(word)
+
     return result
-
-
 def main():
     my_text = 'За результатами дослідження одного англійського університету, не має значення,' \
               ' в якому порядку розташовані букви в слові. Головне, щоб перша і остання букви були на місці. ' \
               'Решта букв можуть слідкувати в повному безладі, все одно текст читається без проблем. ' \
               'Причиною цього є те, що ми не читаємо кожну букву окркмо, а все слово цілком.'
-    print(*pemrtuate(my_text), sep='')
+    print(*pemrtuate(my_text), sep=' ')
 
-    
+
 if __name__ == '__main__':
-    main()    
+    main()

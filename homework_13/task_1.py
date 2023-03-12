6b00acd22bfdb84cfac9dcafcd2d82ae393ea6de
vsrede <vsrede94@gmail.com>
2023-03-12 19:33:22 +0100

    Update task_1.py

--- homework_13/task_1.py
+++ homework_13/task_1.py
@@ -1,19 +1,23 @@
-class Godzilla:
+def main():
+    class Godzilla:
 
-    def __init__(self, stomach_size):
-        self.stomach_size = stomach_size
+        def __init__(self, stomach_size):
+            self.stomach_size = stomach_size
 
-    def eat(self, size_people):
-        occupancy_stomach = self.stomach_size
-        full_stomach = occupancy_stomach * 0.1
-        while occupancy_stomach > full_stomach:
-            occupancy_stomach -= size_people
-            if occupancy_stomach > full_stomach:
-                print('I need more people')
+        def eat(self, size_people):
+            occupancy_stomach = self.stomach_size
+            full_stomach = occupancy_stomach * 0.1
+            while occupancy_stomach > full_stomach:
+                occupancy_stomach -= size_people
+                if occupancy_stomach > full_stomach:
+                    print('I need more people')
 
-        print('I\'m full, I can\'t eat people anymore')
+            print('I\'m full, I can\'t eat people anymore')
+
+    Egor = Godzilla(100)
+
+    Egor.eat(40)
 
 
-Egor = Godzilla(100)
-
-Egor.eat(40)
+if __name__ == '__main__':
+    main()

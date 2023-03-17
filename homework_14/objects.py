eb7270ee0beddd60d4d35826c29d234031fd740d
vsrede <vsrede94@gmail.com>
2023-03-17 20:04:09 +0100

    Create objects.py

---
+++ homework_14/objects.py
@@ -0,0 +1,87 @@
+class Vehicle:
+    species = 'Vehicle'
+
+    def __init__(self, brand, max_speed, color):
+        self.brand = brand
+        self.max_speed = max_speed
+        self.color = color
+
+    def message_tensile_strength(self):
+        tensile_strength = self.max_speed * 1.2
+        return (f'We do not recommend accelerating the vehicle above the maximum speed, '
+                f'the tensile strength at speed {tensile_strength} km/h is 0.5 of the rated')
+
+    def trigger_mechanism(self):
+        return 'Button, key or more complex mechanism'
+
+
+class Air_Vehicle(Vehicle):
+    moving_environment = 'Sky'
+
+    def __init__(self, *args, max_high, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.max_high = max_high
+
+    def message_tensile_strength(self):
+        tensile_strength = self.max_speed * 1.2 * 0.0008
+        return f'We do not recommend accelerating the vehicle above the maximum speed, the tensile strength at speed ' \
+               f'{tensile_strength} mach is 0.5 of the rated.Be careful, ' \
+               f'supersonic affects your health'
+
+    def trigger_mechanism(self):
+        return 'Complex mechanism'
+
+
+class Plane(Air_Vehicle):
+
+    def __init__(self, *args, engine_layout, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.engine_layout = engine_layout
+
+
+class Helicopter(Air_Vehicle):
+    def __init__(self, *args, amount_blades, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.amount_blades = amount_blades
+
+
+class Car(Vehicle):
+
+    def __init__(self, *args, engine_volume, mileage, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.engine_volume = engine_volume
+        self.mileage = mileage
+
+    def trigger_mechanism(self):
+        return 'Key or button'
+
+
+class French_car(Car):
+
+    def __init__(self, *args, suspension, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.suspension = suspension
+
+    @staticmethod
+    def list_auto_repair_shop():
+        return 'Nobody wan\'t repair your car'
+
+
+class Germany_car(Car):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @staticmethod
+    def list_auto_repair_shop(self):
+        message = 'Your car is beautiful, you don\'t need to go there'
+
+        if self.brand == 'BMW':
+            return f'{message}, just change the oil'
+        else:
+            return message
+
+
+
+
+

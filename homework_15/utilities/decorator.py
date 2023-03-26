036aaee5147e2592522be8bf020b03de637a19b7
vsrede <vsrede94@gmail.com>
2023-03-26 19:08:53 +0200

    Create decorator.py

---
+++ homework_15/utilities/decorator.py
@@ -0,0 +1,12 @@
+from utilities.parser import verbose
+
+
+def start_end_decorator(func):
+    def wrapper(*args, **kwargs):
+        if verbose:
+            print("Starting to process your request")
+        func(*args, **kwargs)
+        if verbose:
+            print("Processing is over")
+
+    return wrapper
